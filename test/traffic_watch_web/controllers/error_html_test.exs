631f8806b98acc1aeb384fc81133079f7cdc2ae4
paulsullivanjr <paulsullivan1@gmail.com>
2024-08-09 18:52:00 -0400

    user auth, menu css updates

--- test/traffic_watch_web/controllers/error_html_test.exs
+++ test/traffic_watch_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule TrafficWatchWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(TrafficWatchWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(TrafficWatchWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
